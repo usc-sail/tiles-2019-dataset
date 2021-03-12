56c2d7c3419416c0d710a6f315038919c8d92feb
Karel Mundnich <mundnich@usc.edu>
2021-03-11 17:51:16 -0800

    Process files with a given time frequency

--- src/LAC_info/process_census_data.py
+++ src/LAC_info/process_census_data.py
@@ -10,18 +10,18 @@ class Processor:
         self.unit = unit
         self.shift=shift
 
-    def count(self, df: pd.DataFrame, cutoffs=[6, 17]) -> pd.DataFrame:
+    def count(self, df: pd.DataFrame, cutoffs=[6, 17], freq="1H") -> pd.DataFrame:
         df = df.loc[df[self.location] == self.unit, :]
 
         df = pd.DataFrame({'datetime': df[self.column], self.counter: 1}).sort_values(by='datetime')
-        df = df.resample('1H', on='datetime').sum()
+        df = df.resample(freq, on='datetime').sum()
 
         start_date = (df.index[0] - pd.Timedelta(days=1)).replace(hour=cutoffs[-1])
         end_date = (df.index[-1] + pd.Timedelta(days=1)).replace(hour=cutoffs[0])
         df = pd.concat([
-            pd.DataFrame({'datetime': pd.date_range(start=start_date, end=df.index[0] - pd.Timedelta(hours=1), freq='H'), self.counter: 0}).set_index('datetime'),
+            pd.DataFrame({'datetime': pd.date_range(start=start_date, end=df.index[0] - pd.Timedelta(hours=1), freq=freq), self.counter: 0}).set_index('datetime'),
             df,
-            pd.DataFrame({'datetime': pd.date_range(start=df.index[-1] + pd.Timedelta(hours=1), end=end_date, freq='H'), self.counter: 0}).set_index('datetime'),])
+            pd.DataFrame({'datetime': pd.date_range(start=df.index[-1] + pd.Timedelta(hours=1), end=end_date, freq=freq), self.counter: 0}).set_index('datetime'),])
         df['group'] = df.index.hour.isin(cutoffs).cumsum()
         df = df.reset_index().groupby('group').agg({'datetime': 'first', self.counter: 'sum'})
         if self.shift:
@@ -44,11 +44,11 @@ class Discharges(Processor):
         self.unit = unit
         self.shift = shift
 
-def get_census(file: Path, unit, cutoffs=[6, 17], shift=True) -> pd.DataFrame:
+def get_census(file: Path, unit, cutoffs=[6, 17], shift=True, freq="1H") -> pd.DataFrame:
     df = pd.read_excel(file)
     admissions = Admissions(unit, shift)
     discharges = Discharges(unit, shift)
-    df = (admissions.count(df, cutoffs=cutoffs)).merge(discharges.count(df, cutoffs=cutoffs), on='datetime', how='outer').fillna(0)
+    df = (admissions.count(df, cutoffs=cutoffs, freq=freq)).merge(discharges.count(df, cutoffs=cutoffs, freq=freq), on='datetime', how='outer').fillna(0)
     df.insert(1, 'unit', unit)
     return df
 
@@ -65,14 +65,16 @@ def process_census(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def process_unit(unit, args, save=True):
-    census_2019 = get_census(args.path/'4A 4B Admission Report 2019.xlsx', unit, cutoffs=args.cutoffs, shift=args.shift)
-    census_2020 = get_census(args.path/'4A 4B Admission Report 2020.xlsx', unit, cutoffs=args.cutoffs, shift=args.shift)
+    census = {}
+    for year in [2019, 2020]:
+        census[year] = get_census(args.path/f'4A 4B Admission Report {year}.xlsx', unit, cutoffs=args.cutoffs, shift=args.shift, freq=args.freq)
 
-    census = merge_censuses(census_2019, census_2020)
+    census = merge_censuses(census[2019], census[2020])
     census = process_census(census)
 
     census.set_index('datetime', inplace=True)
-    census.index = census.index.tz_localize(tz="America/Los_Angeles", ambiguous="infer", nonexistent="shift_forward")
+    dst = census.index < pd.to_datetime("2019-11-03T01:00:00", infer_datetime_format=True) # DST backward for 2019
+    census.index = census.index.tz_localize(tz="America/Los_Angeles", ambiguous=dst, nonexistent="shift_forward") # Shifting forward for 2020
 
     if save:
         census.to_csv(args.save/f"LAC_unit{unit}_census.csv", index=True)
--- src/LAC_info/process_deaths.py
+++ src/LAC_info/process_deaths.py
@@ -10,11 +10,14 @@ class Deaths(Processor):
         self.unit = unit
         self.shift = shift
 
-def get_deaths(file: Path, unit, cutoffs=[6, 17], shift=True) -> pd.DataFrame:
+def get_deaths(file: Path, unit, cutoffs=[6, 17], freq="1H", shift=True) -> pd.DataFrame:
     df = pd.read_excel(file)
     deaths = Deaths(unit, shift)
-    df = deaths.count(df, cutoffs=cutoffs)
+    df = deaths.count(df, cutoffs=cutoffs, freq=freq)
     df.insert(1, 'unit', unit)
+    df.set_index('datetime', inplace=True)
+    dst = df.index < pd.to_datetime("2019-11-03T01:00:00", infer_datetime_format=True) # DST backward for 2019
+    df.index = df.index.tz_localize(tz="America/Los_Angeles", ambiguous=dst, nonexistent="shift_forward") # Shifting forward for 2020
     return df
 
 def merge_census_and_deaths(census: pd.DataFrame, deaths: pd.DataFrame):
@@ -25,6 +28,7 @@ if __name__ == "__main__":
     parser.add_argument('path', type=Path, help='Path to folder containing 2019 and 2020 files.')
     parser.add_argument('--save', type=Path, default=Path(__file__).parent.absolute(), help='Path to save files.')
     parser.add_argument('--cutoffs', nargs='*', type=int, default=[6, 17], help='List of cutoff times to compute census separated by spaces in 24-hour format, e.g. 6 17.')
+    parser.add_argument('--freq', type=str, default="1H", help='Resampling frequency of time series (default, 1H. To resample every 30min, use "30T".')
     parser.add_argument('--shift', default=False, action='store_true', help='Shift admissions and discharges to count by the of time window instead of the beginning.')
 
     args = parser.parse_args()
@@ -34,10 +38,12 @@ if __name__ == "__main__":
     df = {}
     for unit in ['4A', '4B']:
         census[unit] = process_unit(unit, args, save=False)
-        deaths[unit] = get_deaths(args.path/'Mortalities_LAC+USC.xlsx', unit, cutoffs=args.cutoffs, shift=args.shift)
+        deaths[unit] = get_deaths(args.path/'Mortalities_LAC+USC.xlsx', unit, cutoffs=args.cutoffs, freq=args.freq, shift=args.shift)
         df[unit] = census[unit].merge(deaths[unit], on=['datetime', 'unit'], how='outer').fillna(0)
 
-    results = pd.concat([df[unit] for unit in ['4A', '4B']]).sort_values(['datetime', 'unit']).set_index('datetime')
-    results.index = results.index.tz_localize(tz="America/Los_Angeles", ambiguous="infer", nonexistent="shift_forward")
+    results = pd.concat([df[unit] for unit in ['4A', '4B']]).reset_index()
+    import pdb; pdb.set_trace()
+    results = results.sort_values(by=['datetime', 'unit']).set_index('datetime')
 
-    results.to_csv(args.save/"LAC+USC_census_and_mortalities.csv", index=True)
+    freqs = {"1H": "1hour", "30T": "30min"}
+    results.to_csv(args.save/f"LAC+USC_census_and_mortalities_{freqs[args.freq]}.csv", index=True)
